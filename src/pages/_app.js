b0d145ab58f63c121f74a956cab36888a8fc4f31
youssef-kobi <yel-kobi@Yel>
2023-11-18 02:41:55 +0100

    finished V1

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,19 +1,25 @@
 import Layout from '@/components/Layout';
 import Transition from '@/components/Transition';
 import '@/styles/globals.css';
-import { AnimatePresence,motion } from 'framer-motion';
+import { AnimatePresence, motion } from 'framer-motion';
+import Head from 'next/head';
 import { useRouter } from 'next/router';
 
 export default function App({ Component, pageProps }) {
-  const router = useRouter()
+  const router = useRouter();
   return (
-    <Layout>
-      <AnimatePresence mode='wait'>
-        <motion.div key={router.route} className='h-full '>
-        <Transition />
-          <Component {...pageProps} />
-        </motion.div>
-      </AnimatePresence>
-    </Layout>
+    <>
+      <Head>
+        <meta name='viewport' content='width=device-width, initial-scale=1' />
+      </Head>
+      <Layout>
+        <AnimatePresence mode='wait'>
+          <motion.div key={router.route} className='h-full '>
+            <Transition />
+            <Component {...pageProps} />
+          </motion.div>
+        </AnimatePresence>
+      </Layout>
+    </>
   );
 }
