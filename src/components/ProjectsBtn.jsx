94736bda4ce0ddb28be0b576393375d919f48941
youssef-kobi <yel-kobi@Yel>
2024-01-03 21:05:35 +0100

    png to webp inmage test

--- src/components/ProjectsBtn.jsx
+++ src/components/ProjectsBtn.jsx
@@ -7,7 +7,7 @@ const ProjectsBtn = () => {
   return (
     <div className='mx-auto xl:mx-0'>
       <Link href={'/work'} className='relative  z-50 w-[135px] h-[135px] lg:w-[185px] lg:h-[185px] flex justify-center items-center bg-circleStar bg-cover bg-center bg-no-repeat group hover:scale-110 duration-300'>
-        <Image src={'/rounded-text.png'} width={101} height={108} alt='rounded text' className='animate-spin-slow w-full h-full max-w-[101px] max-h-[108px] xl:max-w-[141px] xl:max-h-[148px]' />
+        <Image src={'/rounded-text.webp'} width={101} height={108} alt='rounded text' className='animate-spin-slow w-full h-full max-w-[101px] max-h-[108px] xl:max-w-[141px] xl:max-h-[148px]' />
         <HiArrowRight className='absolute text-4xl group-hover:translate-x-2 transition-all duration-300' />
       </Link>
     </div>
--- src/components/TopLeftImg.jsx
+++ src/components/TopLeftImg.jsx
@@ -4,7 +4,7 @@ import React from 'react'
 const TopLeftImg = () => {
   return (
     <div className='absolute left-0 top-0 mix-blend-color-dodge z-10 w-52 xl:w-[400px] opacity-50'>
-      <Image src='/top-left-img.png' alt='top-left-img' width={400} height={400} priority />
+      <Image src='/top-left-img.webp' alt='top-left-img' width={400} height={400} priority />
     </div>
   )
 }
