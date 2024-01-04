91c1fb7533feea479db11ea0b338f2e63729ce59
youssef-kobi <yel-kobi@Yel>
2024-01-04 14:16:30 +0100

    lazy loading

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,5 @@
 import Layout from '@/components/Layout';
-import Transition from '@/components/Transition';
+// import Transition from '@/components/Transition';
 import '@/styles/globals.css';
 import { AnimatePresence, motion } from 'framer-motion';
 import Head from 'next/head';
@@ -7,6 +7,12 @@ import { useRouter } from 'next/router';
 
 export default function App({ Component, pageProps }) {
   const router = useRouter();
+  // Dynamically import Transition component with lazy loading
+const Transition = dynamic(() => import('@/components/Transition'), {
+  loading: () => <p>Loading...</p>, // Optional loading component
+  ssr: false, // Disable SSR for this component
+});
+
   return (
     <>
       <Head>
